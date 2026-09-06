d8f77ad4ceeccc84ba786c3acdcd43dd1a86f8c4
mojahid018 <mojahidansari62030@gmail.com>
2026-09-06 10:04:42 +0530

    Time: 11 ms (35.19%), Space: 23 MB (24.39%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
+
+        // Returns total hours needed at speed k
+    long long hours(vector<int>& piles, int k) {
+        long long hrs = 0;
+
+        for (int i = 0; i < piles.size(); i++) {
+            hrs += piles[i] / k;
+
+            if (piles[i] % k != 0)
+                hrs++;
+        }
+
+        return hrs;
+    }
     int minEatingSpeed(vector<int>& piles, int h) {
 
-        int n = piles.size();
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
+
         int res = -1;
-        while(low<= high){
-            int mid = (low+high)/2;
-            long long hrs = 0;
-            for(int i = 0; i < n; i++){
-                hrs = hrs+piles[i]/mid;
-                if(piles[i] % mid != 0)
-                    hrs++;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (hours(piles, mid) > h) {
+                // Speed is too slow
+                low = mid + 1;
             }
-            if(hrs > h)
-                low = mid+1;
-            else{
+            else {
+                // Speed works, try smaller
                 res = mid;
-                high = mid-1;
+                high = mid - 1;
             }
         }
 
